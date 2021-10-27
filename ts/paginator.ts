474a5d4711e4a9e8e0b689c76180c6697bdac620
JezerM <amyuki4@icloud.com>
2021-10-27 12:38:34 -0600

    Removed dependencies and improved paginator

--- ts/paginator.ts
+++ ts/paginator.ts
@@ -1,313 +1,295 @@
-import 'ansi-escapes'
-import ansiEscapes from 'ansi-escapes'
-
-import { fromEvent } from 'rxjs'
-import { map } from 'rxjs/operators'
-import readline from 'readline'
-import chalk from 'chalk';
-import sliceAnsi from 'slice-ansi'
-import stripAnsi from 'strip-ansi'
+import ansiRegex from "ansi-regex";
+import chalk, { Chalk } from "chalk";
+import stripAnsi from "strip-ansi";
+import readline from "readline";
 
 interface pageOptions {
-  /**
-   * Defines the max number of lines to show in console. Use a positive integer.
-   */
-  pageSize?: number,
-  /**
-   * Shows an static message above the text.
-   */
-  message?: string
-  /**
-   * Shows an static, dim message for help, like '(Use arrow keys)'
-   */
-  suffix?: string
-  /**
-   * Shows a message for help to continue executing the code. 'Press return button to exit'
-   */
-  exitMessage?: string
-  /**
-   * If true, defines if is neccesary to go trough all the pages to continue
-   */
-  read_to_return?: boolean,
-  /**
-   * The WriteStream to write on, such as process.stdout
-   */
-  writable?: NodeJS.WriteStream
-  /**
-   * The ReadStream to read on, such as process.stdin
-   */
-  readable?: NodeJS.ReadStream
-  /**
-   * Sets the maximum number of columns and rows.
-   */
-  terminal?: { columns: number, rows: number }
+  page_size?: number;
+  message?: string;
+  suffix?: string;
+  exit_message?: string;
+  read_to_return?: boolean;
+  writable?: NodeJS.WriteStream;
+  readable?: NodeJS.ReadStream;
+  terminal?: { rows?: number; cols?: number };
+  style?: { enum?: Chalk };
 }
 
-const defaultOptions = {
-  message: 'Paginated text:',
-  suffix: '(Use arrow keys)',
-  exitMessage: 'Press return button to exit',
+const defaultOptions: Required<pageOptions> = {
+  page_size: 5,
+  message: "Paginated text:",
+  suffix: "(Use arrow keys)",
+  exit_message: "Press return key to exit",
   read_to_return: true,
-  pageSize: 3,
-  writable: process.stdout,
   readable: process.stdin,
+  writable: process.stdout,
+  terminal: { rows: 0, cols: 0 },
+  style: {},
+};
+
+function limit(val: number, min: number, max: number) {
+  if (val > max - 1) {
+    return (val = max);
+  } else if (val <= min) {
+    return (val = min);
+  }
+  return val;
 }
 
 /**
- * Allows to paginate or split the text in the console, moving the page with arrow keys, awaiting for user action to continue the code.
- * @async It's preffered to use Await/Async to avoid executing next code without waiting user action.
+ * Allows to paginate or trunk the text in the console, moving it with arrow keys and awaiting for user interaction to continue the code.
+ * @async It's preffered to use await/async to avoid executing next code without user action
+ * @class Initialize a new instance of Paginator
  */
 class Paginator {
-  private supSaved: string = ''
-  private savedText: string = ''
-  private _actualText: string = ''
-  private _position: number = 0
-  private once: boolean = true
+  private _saved_text: string = "";
+  private _stripped_text: string[] = [];
+  private _actual_text: string = "";
+  private _position: number = 0;
+  private _opts: Required<pageOptions> = defaultOptions;
 
-  private _opts: pageOptions = {};
-  private savedSize: number | undefined
-  public get opts(): pageOptions {
-    return this._opts;
+  private get rows(): number {
+    return this.opts.terminal.rows || this.opts.writable.rows;
   }
-  public set opts(v: pageOptions) {
-    this._opts = v;
+  private get columns(): number {
+    return this.opts.terminal.cols || this.opts.writable.columns;
+  }
+
+  public get saved_text() {
+    return this._saved_text;
+  }
+  public get actual_text() {
+    return this._actual_text;
   }
 
   public get pages() {
-    return this.savedText.split('\n').length - (this.opts.pageSize || defaultOptions.pageSize)
+    return Math.max(this._stripped_text.length - this.opts.page_size, 0);
   }
 
-  public get position() {
-    return this._position
+  public get position(): number {
+    return this._position;
   }
   public set position(v: number) {
-    this._position = v
-    if (this._position > this.pages - 1) {
-      this._position = this.pages
-    }
-    if (this._position <= 0) {
-      this._position = 0
-    }
-    this.updateText()
+    this._position = limit(v, 0, this.pages);
   }
 
-  public get actualText() {
-    return this._actualText
+  public get opts(): Required<pageOptions> {
+    return this._opts;
   }
 
-  constructor(options?: pageOptions) { this.options(options) }
+  constructor(options?: pageOptions) {
+    this.options(options);
+  }
+
+  public options(opts?: pageOptions): this {
+    this.opts.page_size = opts?.page_size ?? this.opts.page_size;
+    this.opts.message = opts?.message ?? this.opts.message;
+    this.opts.suffix = opts?.suffix ?? this.opts.suffix;
+    this.opts.exit_message = opts?.exit_message ?? this.opts.exit_message;
+    this.opts.read_to_return = opts?.read_to_return ?? this.opts.read_to_return;
+    this.opts.readable = opts?.readable ?? this.opts.readable;
+    this.opts.writable = opts?.writable ?? this.opts.writable;
+    this.opts.terminal = opts?.terminal ?? this.opts.terminal;
+    this.opts.style = opts?.style ?? this.opts.style;
+    return this;
+  }
+
+  private exit(): void {
+    if (this.opts.readable.isTTY) this.opts.readable.setRawMode(false);
+    this.opts.readable.pause();
+    this._saved_text = this._actual_text = "";
+    this._position = 0;
+    this.opts.writable.write("\x1b[H\x1b[J");
+    this.opts.writable.write("\x1b[?25h");
+  }
+  public end() {
+    this.exit();
+    return this;
+  }
+
+  private strip_text(text: string): string[] {
+    let orig_lines: string[] = text.split("\n");
+    let lines: string[] = [];
+    let ansi_codes: string[] = [];
+
+    for (let p = 0; p < orig_lines.length; p++) {
+      orig_lines[p] = orig_lines[p].replace(/\r/g, "");
+      let ansi_line = orig_lines[p];
+
+      let wordArr: string[] = ansi_line.split(" ");
+
+      let line_number: string = this.opts.style.enum
+        ? this.opts.style.enum(lines.length + 1)
+        : "";
+      let push_line: string = "" + line_number + " ";
+      for (let i = 0; i < wordArr.length; i++) {
+        let word: string = wordArr[i];
+        let next_word: string = i + 1 < wordArr.length ? wordArr[i + 1] : "";
+
+        let matched = word.match(ansiRegex());
+        matched?.forEach((v) => {
+          ansi_codes.push(v);
+        });
+
+        let line_number: string = this.opts.style.enum
+          ? this.opts.style.enum(lines.length + 2)
+          : "";
+
+        let line_length: number =
+          stripAnsi(push_line).length +
+          stripAnsi(word).length +
+          stripAnsi(line_number).length;
+
+        if (line_length >= this.columns) {
+          lines.push(push_line);
+          push_line = "" + line_number + " " + ansi_codes.join("");
+        }
+        push_line += word + " ";
+        if (i + 1 >= wordArr.length) {
+          push_line += next_word;
+          lines.push(push_line);
+          push_line = "" + line_number + " " + ansi_codes.join("");
+        }
+      }
+    }
+
+    return lines;
+  }
+
+  private manage_keys(data: Buffer): boolean {
+    let keys: number[] = data.toJSON().data;
+    if (keys.length == 0) return false;
+
+    if (keys[0] == 3) {
+      // 'Ctrl + c' key
+      this.exit();
+      return true;
+    } else if (keys[0] == 27 && keys[1] == 91) {
+      if (keys[2] == 66) {
+        // Down
+        this.position++;
+      } else if (keys[2] == 65) {
+        // Up
+        this.position--;
+      }
+    } else if (keys[0] == 106) {
+      // 'j' key
+      this.position++;
+    } else if (keys[0] == 107) {
+      // 'k' key
+      this.position--;
+    }
+
+    if (this.opts.read_to_return == true) {
+      if ((keys[0] == 13 || keys[0] == 10) && this.position >= this.pages) {
+        this.exit();
+        return true;
+      }
+    } else {
+      if (keys[0] == 13) {
+        this.exit();
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public update_text(): void {
+    let stripped_arr: string[] = this.strip_text(this._saved_text);
+
+    this._stripped_text = stripped_arr;
+    this._actual_text = "";
+
+    stripped_arr.forEach((line, ind) => {
+      if (
+        ind >= this.position &&
+        ind <= this.opts.page_size + this.position - 1
+      ) {
+        this._actual_text += line + "\n";
+      }
+    });
+
+    let message = chalk`{bold ${this.opts.message}} {dim ${this.opts.suffix}}`;
+
+    let exitMessageLength = this.opts.exit_message.split("\n").length;
+    let moveToExitMessage = `\x1b[${this.rows - exitMessageLength};1H`;
+    let exitMessage = "";
+
+    if (this.opts.read_to_return == true) {
+      exitMessage =
+        this.position >= this.pages
+          ? chalk`{blueBright.bold [${this.opts.exit_message}]}`
+          : chalk`{dim [${this.opts.exit_message}]}`;
+    } else {
+      exitMessage = chalk`{blueBright.bold [${this.opts.exit_message}]}`;
+    }
+    let pages = chalk`{yellowBright.bold ${this.position + 1}/${
+      this.pages + 1
+    }}`;
+
+    let final = `${message}\n\n${this._actual_text}\x1b[0m${moveToExitMessage}${exitMessage}\x1b[E${pages}`;
+
+    this.opts.writable.write("\x1b[H\x1b[J");
+    this.opts.writable.write(final);
+  }
 
   /**
-   * Capture key arrows for moving the text UP and DOWN with a determinate PageSize.
-   * Use Message option for show an static text above the text.
+   * Captures key arrows to move the text UP and DOWN with a determinate `page_size`.
+   * Use `message` option to show an static text above the text.
    * @param {string} text Defines the text to split up and fit in the console.
    * @param {pageOptions | undefined} options Defines the options.
-   * @async Uses Promises, awaiting for the Return key pressed.
+   * @async Uses promises, awaiting for the Return key to be pressed.
    * @returns Promise, resolving in Boolean True.
    */
   public async print(text: string, options?: pageOptions): Promise<this> {
-    this.options(options)
+    this.options(options);
 
-    if (this.opts.pageSize && parseFloat(this.opts.pageSize.toFixed(0)) != this.opts.pageSize) {
-      throw Error('You cant pass a floating point number as pageSize')
-    } else if (this.opts.pageSize && this.opts.pageSize <= 0) {
-      throw Error('You cant pass an equal or below 0 number as pageSize')
+    if (
+      this.opts.page_size &&
+      parseFloat(this.opts.page_size.toFixed(0)) != this.opts.page_size
+    ) {
+      throw Error("You cant pass a floating point number as pageSize");
+    } else if (this.opts.page_size && this.opts.page_size <= 0) {
+      throw Error("You cant pass an equal or below 0 number as pageSize");
     }
 
-    this.supSaved = this.savedText = this._actualText = text
+    this._saved_text = text;
 
-    this.updateText()
-    this.once = false
+    this.update_text();
 
-    await new Promise((resolve, reject) => {
-      this.opts.readable?.setRawMode(true)
-      readline.emitKeypressEvents(this.opts.readable || defaultOptions.readable)
-      const obs = fromEvent(this.opts.readable?.resume() || defaultOptions.readable.resume(), 'keypress')
-      var piped = obs.pipe(
-        map((x: any) => x[1])
-      )
-      var sus = piped.subscribe((key: any) => {
-        // On Ctrl+C, process.exit()
-        if (key && key.ctrl && key.name == 'c') {
-          resolve(true)
-          this.exit()
-          sus.unsubscribe()
-          process.exit()
+    if (this.opts.readable.isTTY) this.opts.readable.setRawMode(true);
+    this.opts.readable.resume();
+    this.opts.writable.write("\x1b[?25l");
+
+    readline.emitKeypressEvents(this.opts.readable);
+
+    await new Promise((resolve) => {
+      let running = true;
+
+      let on_data = (data: Buffer) => {
+        if (this.manage_keys(data) == true) {
+          running = false;
+          resolve(true);
+          this.opts.readable.off("data", on_data);
+          return;
         }
-        // On Down arrow key, moves 
-        if (key.name == 'down') {
-          this.down()
-        } else if (key.name == 'up') {
-          this.up()
-        }
-        // If position is equal to last page and key pressed is Return, exits
-        if (this.opts.read_to_return == true) {
-          if (this.opts.pageSize && key.name == 'return' && this.position >= this.savedText.split('\n').length - this.opts.pageSize) {
-            resolve(true)
-            this.exit()
-            sus.unsubscribe()
-          }
-        } else {
-          if (key.name == 'return') {
-            resolve(true)
-            this.exit()
-            sus.unsubscribe()
-          }
-        }
-      })
-    })
-    return this
-  }
+        this.update_text();
+      };
+      let on_resize = () => {
+        if (running) this.update_text();
+        else this.opts.writable.off("resize", on_resize);
+      };
 
-  /**
-   * Defines the options
-   */
-  public options(options?: pageOptions) {
-    this.opts.message = options?.message || (this.opts.message || defaultOptions.message)
-    this.opts.exitMessage = options?.exitMessage || (this.opts.exitMessage || defaultOptions.exitMessage)
-    this.opts.pageSize = options?.pageSize || (this.opts.pageSize || defaultOptions.pageSize)
-    this.opts.read_to_return = options?.read_to_return != undefined ? options?.read_to_return : (this.opts.read_to_return || defaultOptions.read_to_return)
-    this.opts.suffix = options?.suffix || (this.opts.suffix || defaultOptions.suffix)
-    this.opts.writable = options?.writable || (this.opts.writable || defaultOptions.writable)
-    this.opts.readable = options?.readable || (this.opts.readable || defaultOptions.readable)
-    this.opts.terminal = options?.terminal || this.opts.terminal || undefined
-    return this
-  }
+      this.opts.readable.on("data", on_data);
+      this.opts.writable.on("resize", on_resize);
+    });
+    this.opts.writable.write("\x1b[H\x1b[J");
+    this.opts.writable.write("\x1b[?25h");
 
-  /**
-   * Prepares the console to exit
-   */
-  private exit() {
-    this.opts.readable?.setRawMode(false)
-    this.opts.readable?.pause()
-    this.opts.pageSize = this.savedSize || (this.opts.pageSize || defaultOptions.pageSize)
-    this.savedSize = undefined
-    this.savedText = this._actualText = ''
-    this._position = 0
-    this.once = true
-    clear(this.opts.writable || defaultOptions.writable)
-    this.opts.writable?.write(ansiEscapes.cursorShow)
-  }
-
-  public end() {
-    this.exit()
-    return this
-  }
-  /**
-   * Moves the page an step down
-   */
-  private down() {
-    this.position++
-    this.updateText()
-  }
-  /**
-   * Moves the page an step up
-   */
-  private up() {
-    this.position--
-    this.updateText()
-  }
-
-  private columns(): number {
-    return this.opts.terminal?.columns || this.opts.writable?.columns || defaultOptions.writable.columns
-  }
-  private rows(): number {
-    return this.opts.terminal?.rows || this.opts.writable?.rows || defaultOptions.writable.rows
-  }
-  /**
-   * Updates the @var actualText, getting the actual position on page and printing it in the console
-   */
-  private updateText() {
-    this.format()
-    var splitted = this.savedText.split('\n')
-    this._actualText = ''
-    var final = ''
-    splitted.forEach((value, index) => {
-      if (index >= this.position && index <= (this.opts.pageSize || defaultOptions.pageSize) - 1 + this.position) {
-        this._actualText += value + '\n'
-      }
-    })
-    clear(this.opts.writable || defaultOptions.writable)
-
-    readline.cursorTo(this.opts.writable || defaultOptions.writable, 0, 0)
-
-    var message = `${chalk.bold(this.opts.message)} ${this.once ? chalk.dim(this.opts.suffix) : ''}`
-
-    var exitMessageLength = this.opts.exitMessage ? this.opts.exitMessage.split('\n').length : 0
-
-    var exitMessage = ''
-
-    var moveToExitMessage = ansiEscapes.cursorTo(0, this.rows() - exitMessageLength - 1)
-    if (this.opts.read_to_return == true) {
-      exitMessage = this.position >= this.pages ? chalk`{blueBright.bold [${this.opts.exitMessage}]}` : chalk`{dim.bold [${this.opts.exitMessage}]}`
-    } else {
-      exitMessage = chalk`{blueBright.bold [${this.opts.exitMessage}]}`
-    }
-
-    var moveDown = ansiEscapes.cursorNextLine
-    var pages = chalk`{yellowBright.bold ${this.position + 1}/${this.pages + 1}}`
-
-    final = `${message}\n\n${this._actualText}${moveToExitMessage}${exitMessage}${moveDown}${pages}`
-    this.opts.writable?.write(final)
-  }
-  /**
-   * Formats the @var savedText, splitting it to fit well in the console.
-   */
-  private format() {
-    var maxColumns = this.columns()
-    var linesArr = this.supSaved.split('\n')
-    var finalText = ''
-    linesArr.forEach((value, indexLines, arrLines) => {
-      var wordArr = value.split(' ')
-      var line = ''
-      if (indexLines != 0) {
-        finalText += '\n'
-      }
-      var last = 0
-      wordArr.forEach((valueW, indexW, arr) => {
-        var actual = stripAnsi(valueW) // Erases ansi codes in every word
-        var finalWord = sliceAnsi(value, last + indexW, last + indexW + actual.length) // Slices the real text, keeping ansi codes
-        last += actual.length
-        arr[indexW] = finalWord
-      })
-
-      wordArr.forEach((word, indexWord, arrWords) => {
-        var lolLine = stripAnsi(line)
-        var lolWord = stripAnsi(word)
-
-        if ((lolLine + lolWord).length > maxColumns) {
-          if (lolLine[lolLine.length - 1] === ' ') { // If last character of line is an empty space, delete it
-            line = sliceAnsi(line, 0, lolLine.length - 1)
-          }
-          finalText += line + '\n'
-          line = ''
-        }
-        line += word + ' '
-      })
-
-      if (line.length > 0) {
-        finalText += line
-      }
-    })
-    var lines = finalText.split('\n')
-    this.savedSize = this.savedSize || (this.opts.pageSize || defaultOptions.pageSize)
-    if ((this.opts.pageSize || defaultOptions.pageSize) >= lines.length) {
-      this.opts.pageSize = finalText.split('\n').length
-    }
-    this.savedText = finalText
+    return this;
   }
 }
-/**
- * Just clears the console, hiding the cursor
- */
-function clear(writable: NodeJS.WriteStream) {
-  writable.write(ansiEscapes.cursorTo(0, 0))
-  writable.write(ansiEscapes.clearTerminal)
-  writable.write(ansiEscapes.cursorHide)
-}
 
-export {
-  Paginator,
-  pageOptions,
-  defaultOptions
-}
+export { Paginator, pageOptions, defaultOptions };
+
+// vim: expandtab shiftwidth=2 tabstop=2
